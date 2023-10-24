def9ba76a26becc7ef6907e01c179b82b388d939
vcjuliocesar <vcjuliocesar@outlook.com>
2023-10-23 19:03:55 -0600

    initialize constants

--- src/domain/value_objects.py
+++ src/domain/value_objects.py
@@ -4,12 +4,15 @@
 import uuid
 
 userId = uuid.UUID
-_registered = False
+_registered:bool = False
 
 INITIALIZE_BALANCE_AMOUNT:int = 11000
 INITIALIZE_CREDIT_BALANCE_AMOUNT:int = 500
 EMPTY_BALANCE:int = 0
 MAX_AMOUNT_AVAILABLE:int = 100000000
 MIN_AMOUNT_AVAILABLE:int = 10
+
+FEE:float = 0.05
+LIMIT_CREDIT:int = 400000
 
     
